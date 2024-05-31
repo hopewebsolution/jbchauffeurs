a5f57843c4c946532a0fc26fcf6208b8a8c07f79
hopewebsolution <loginaccesscontrol@gmail.com>
2024-05-31 10:04:44 +0530

    edit profile

--- resources/views/operatorforgotpassword.blade.php
+++ resources/views/operatorforgotpassword.blade.php
@@ -6,7 +6,7 @@
         <div class="col-lg-8 col-md-12">
             <div class="member-login-center">
                 <div class="login-box form-heading">
-                    <h3>Password recovery</h3>
+                    <h3>Reset Password</h3>
                     <!-- @if ($errors->has('sendResetLinkEmail'))
                     <div class="alert alert-danger">
                         {{ $errors->first('sendResetLinkEmail') }}
@@ -26,7 +26,7 @@
                     <form action="{{ route('password.email.link') }}" method="post">
                         @csrf
                         <div class="form-group text-left">
-                            <label for="account-id">Please enter your Account ID</label>
+                            <label for="account-id">Please enter your Email ID</label>
                             <input type="text" class="form-control" id="email"  placeholder="Enter the email" name="email" >
                             <span class="invalid-feedback">This field is required.</span>
                             @error('email')
