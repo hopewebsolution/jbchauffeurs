fffad8f350e052902a821b12ba79cbb3fec54740
gaurav0344 <gaurav.gurjar@webrockmedia.com>
2024-07-23 19:19:03 +0530

    commmits

--- resources/views/operatorregister.blade.php
+++ resources/views/operatorregister.blade.php
@@ -402,9 +402,8 @@
                                                         <input type="text" placeholder="Authorised contact person"
                                                             class="form-control required" required
                                                             name="authorised_contact_person" value=""
-                                                            
+                                                            minlength="10" maxlength="10" pattern="[1-9]{1}[0-9]{9}"
                                                             class="ys-field">
-                                                            <!-- minlength="10" maxlength="10" pattern="[1-9]{1}[0-9]{9}" -->
                                                         <span class="invalid-feedback">This field is required.</span>
                                                     </div>
                                                     <div class="form-group col-md-6 text-left">
